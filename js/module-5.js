98b758e84a788ea75b020c8d8fafea579340d637
IrinaZamkova <58607579+IrinaZamkova@users.noreply.github.com>
2020-05-22 01:06:22 +0300

    Update module-5.js

--- js/module-5.js
+++ js/module-5.js
@@ -1,36 +1,33 @@
-
 "use strict";
 const deliveryCountry = prompt("Please select delivery country");
 
-let cost;
-if(deliveryCountry===null){
-  console.log("Canceled by user")
-}else{
-const deliveryCountryUpper = deliveryCountry.toUpperCase();
-switch (deliveryCountryUpper) {
-  case "CHINA":
-    cost = 100;
-    alert(`Delivery in ${deliveryCountryUpper} will cost ${cost} credits.`);
-    break;
+if (deliveryCountry === null) {
+  console.log("Canceled by user");
+} else {
+  switch (deliveryCountry.toUpperCase()) {
+    
+    case "CHINA":
+      alert(`Delivery in ${deliveryCountry} will cost 100 credits.`);
+      break;
 
-  case "CHILI":
-    cost = 250;
-    alert(`Delivery in ${deliveryCountryUpper} will cost ${cost} credits.`);
-    break;
+    case "CHILI":
+      alert(`Delivery in ${deliveryCountry} will cost 250 credits.`);
+      break;
 
-  case " AUSTRALIA":
-    cost = 170;
-    alert(`Delivery in ${deliveryCountryUpper} will cost ${cost} credits.`);
-    break;
-  case " iINDIA":
-    cost = 80;
-    break;
-  case " JAMAICA":
-    cost = 120;
-    alert(`Delivery in ${deliveryCountryUpper} will cost ${cost} credits.`);
-    break;
+    case " AUSTRALIA":
+      alert(`Delivery in ${deliveryCountry} will cost 170 credits.`);
+      break;
 
-  default:
-    alert("Delivery is not available in your country");
-    break;
-}}
+    case " iINDIA":
+      alert(`Delivery in ${deliveryCountry} will cost 80 credits.`);
+      break;
+
+    case " JAMAICA":
+      alert(`Delivery in ${deliveryCountryUpper} will cost 120 credits.`);
+      break;
+
+    default:
+      alert("Delivery is not available in your country");
+      break;
+  }
+}
