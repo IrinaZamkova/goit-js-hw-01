dcd9ad6c7c3eab3b316c2a2780e3e70aaa6e2a13
IrinaZamkova <58607579+IrinaZamkova@users.noreply.github.com>
2020-05-22 00:06:25 +0300

    Update module-2.js

--- js/module-2.js
+++ js/module-2.js
@@ -1,8 +1,8 @@
 "use strict";
 
 const total = 100;
-const ordered = 80;
-const massage =
-  ordered <= total
-    ? console.log("Заказ оформлен, с вами свяжется менеджер.")
-    : console.log("На складе недостаточно товаров!");
+const ordered = 50;
+if(ordered <= total){
+console.log("Заказ оформлен, с вами свяжется менеджер.");
+}else
+console.log("На складе недостаточно товаров!");
