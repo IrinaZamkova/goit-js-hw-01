710a26560ad13978ba2c1d1308330f77ba7c5803
IrinaZamkova <58607579+IrinaZamkova@users.noreply.github.com>
2020-05-23 15:31:25 +0300

    Update module-2.js

--- js/module-2.js
+++ js/module-2.js
@@ -1,8 +1,6 @@
 "use strict";
-
 const total = 100;
 const ordered = 50;
-if(ordered <= total){
-console.log("Заказ оформлен, с вами свяжется менеджер.");
-}else
-console.log("На складе недостаточно товаров!");
+if (ordered <= total) {
+  console.log("Заказ оформлен, с вами свяжется менеджер.");
+} else console.log("На складе недостаточно товаров!");
