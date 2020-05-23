edd6219a660f07b4e74920e6970b57f8273bcf2b
IrinaZamkova <58607579+IrinaZamkova@users.noreply.github.com>
2020-05-23 15:28:28 +0300

    Update module-4.js

--- js/module-4.js
+++ js/module-4.js
@@ -3,14 +3,18 @@
 const credits = 23580;
 const pricePerDroid = 3000;
 let ordered = prompt("Укажите кол-во товара:");
-let totalPrice = pricePerDroid * ordered;
 
 if (ordered === null) {
   console.log("Отменено пользователем!");
-} else if (totalPrice <= credits) {
-  console.log(
-    `Вы купили ${ordered} дроидов, на счету осталось ${credits - totalPrice} кредитов.`,
-  );
 } else {
-  console.log("Недостаточно средств на счету!");
+  let totalPrice = pricePerDroid * ordered;
+  if (totalPrice <= credits) {
+    console.log(
+      `Вы купили ${ordered} дроидов, на счету осталось ${
+        credits - totalPrice
+      } кредитов.`,
+    );
+  } else {
+    console.log("Недостаточно средств на счету!");
+  }
 }
